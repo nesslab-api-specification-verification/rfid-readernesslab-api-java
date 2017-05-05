1fdd2f590976a632168191f7efab20268ef71356
Dannylo <Dannylo@dannylo>
2017-05-05 17:02:32 -0300

    Documentação da classe ConnectReader.

--- src/api/reader/nesslab/utils/ConnectReader.java
+++ src/api/reader/nesslab/utils/ConnectReader.java
@@ -7,6 +7,9 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * The class is responsible for open connection and to send messages for the hardware Reader Nesslab. 
+ * */
 public class ConnectReader {
 	
 	private Socket echo	;
@@ -17,6 +20,14 @@ public class ConnectReader {
 	private ConnectReader() {
 	}
 	
+	/**
+	 * Method used for to return a connection with the reader.
+	 * The method is used exclusively for API Facade. 
+	 * @param ip is the IP of Reader.
+	 * @param port is the port of connection with o Reader, by default is 5578.
+	 * @throws UnknownHostException Is trown when the host not found.
+	 * @throws IOException Is trown when any failure I/O ocurred.
+	 * */
 	/* Singleton */
 	public synchronized static ConnectReader getInstance(String ip, int port) 
 			throws UnknownHostException, IOException{
@@ -34,23 +45,44 @@ public class ConnectReader {
 			return connectReader;
 		}
 	}
-	
+	/**
+	 * Method used for to send a message for the reader, following the specification of proprietary protocol.
+	 * The method is used exclusively for API Facade. 
+	 * @param message is the message according with the protocol. 
+	 * **/
 	public void send(String message){
 		this.out.println(message);
 	}
 	
+	/**
+	 * Method used for to return a response of hardware reader.
+	 * @return response of hardware reader. 
+	 * */
 	public String getResponse() throws IOException{
 			return in.readLine();
 	}
 	
+	/**
+	 * Method used for to return if exists any response in buffer of reader.
+	 * @return true if exists any response, false if not exist. 
+	 * @throws IOException Is trown when any failure I/O ocurred.
+	 * */
 	public boolean hasResponse() throws IOException{
 		return in.read() > 1? true: false;
 	}
 	
+	/**
+	 * Method used for to return the socket of connection with the reader.
+	 * @return Socket connection with reader.  
+	 * */
 	public Socket getConnection(){
 		return echo;
 	}
 	
+	/**
+	 * Method used for to close connection with the hardware reader.
+	 * @throws IOException Is trown when any failure I/O ocurred.
+	 * */
 	public void closeConnection() throws IOException{
 		out.close();
 		in.close();
@@ -61,11 +93,11 @@ public class ConnectReader {
 		this.echo = echo;
 	}
 	
-	public void setOut(PrintWriter out){
+	private void setOut(PrintWriter out){
 		this.out = out;
 	}
 	
-	public void setIn(BufferedReader in){
+	private void setIn(BufferedReader in){
 		this.in = in;
 	}
 	
